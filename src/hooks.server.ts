62c4a9c89a0ec4a71b6b000ce08ffef5f34ee1c4
jill64 <intents.turrets0h@icloud.com>
2023-12-09 23:57:51 +0900

    add hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,8 @@
+import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
+
+const { onHandle, onError } = init(
+  'https://f476ffd69d9fed59ce80394a73f07bac@o4505814639312896.ingest.sentry.io/4506366354194432'
+)
+
+export const handle = onHandle()
+export const handleError = onError()
