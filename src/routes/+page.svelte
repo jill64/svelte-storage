3b14224d317df487aa399f2d387db70c17ce8707
jill64 <intents.turrets0h@icloud.com>
2023-12-11 00:41:53 +0900

    chore: refactor demo with npm-demo-layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,19 @@
 <script lang="ts">
-  import { browser } from '$app/environment'
   import { storage } from '$lib'
 
   const text = storage('page')
 </script>
 
-<svelte:head>
-  {#if browser}
-    <meta data-testid="hydrated" />
-  {/if}
-</svelte:head>
-
 <input bind:value={$text} />
 
 <style>
-  @media (prefers-color-scheme: dark) {
-    :global(body),
-    input {
-      background: #161616;
-      color: whitesmoke;
-    }
-  }
   input {
     border: 1px solid #ccc;
     border-radius: 4px;
     padding: 0.5rem;
     width: 90%;
     font-size: large;
+    background: inherit;
+    color: inherit;
   }
 </style>
