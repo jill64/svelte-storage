2d69513f4e8f876d724886c7491f6eafa753e9c2
jill64 <intents.turrets0h@icloud.com>
2023-12-12 21:52:56 +0900

    chore: refactor with npm-demo-layout

---
+++ src/routes/code.ts
@@ -0,0 +1,13 @@
+export const code = /* html */ `
+<script>
+  import { storage } from '@jill64/svelte-storage'
+
+  const local = storage('localStorage-key')
+  const session = storage('sessionStorage-key', {
+    sessionStorage: true
+  })
+</script>
+
+Persisted in LocalStorage   <input bind:value={$local} />
+Persisted in SessionStorage <input bind:value={$session} />
+`.trim()
