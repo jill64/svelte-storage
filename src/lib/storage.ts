cd520361f946005c08816e0bed18c17e81067c48
jill64 <intents.turrets0h@icloud.com>
2024-06-27 21:54:07 +0900

    minor: fix store reactivity

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,6 +1,6 @@
 import { typedStorage } from '@jill64/typed-storage'
 import type { Options } from '@jill64/typed-storage/types'
-import { readable, type Writable } from 'svelte/store'
+import { writable, type Writable } from 'svelte/store'
 import { string, type Serde } from './serde'
 
 export const storage: {
@@ -8,18 +8,30 @@ export const storage: {
   (key: string, options?: Options): Writable<string>
 } = <T>(key: string, arg?: Serde<T> | Options, opts?: Options): Writable<T> => {
   const isSerdeArg = arg && 'serialize' in arg && 'deserialize' in arg
+  const serde = (isSerdeArg ? arg : string) as Serde<T>
 
-  const { get, set, subscribe } = typedStorage(
+  const { get, set, addListener } = typedStorage(
     key,
-    (isSerdeArg ? arg : string) as Serde<T>,
+    serde,
     opts ?? (isSerdeArg ? undefined : arg)
   )
 
-  const store = readable(get(), subscribe)
+  const store = writable(get(), (set) =>
+    addListener((event) => {
+      set(serde.deserialize(event.newValue ?? ''))
+    })
+  )
 
   return {
     subscribe: store.subscribe,
-    set,
-    update: (updater) => set(updater(get()))
+    set: (value: T) => {
+      set(value)
+      store.set(value)
+    },
+    update: (updater) => {
+      const value = updater(get())
+      set(value)
+      store.set(value)
+    }
   }
 }
